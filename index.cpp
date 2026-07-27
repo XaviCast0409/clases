c76935c1452f0020c45db40247ddbc35ab977194
XaviCast0409 <168607424+XaviCast0409@users.noreply.github.com>
2026-07-27 10:53:12 -0500

    primer trabajo

---
+++ index.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    switch (2)
+    {
+        case 1:
+            cout << "Hello, World!" << endl;
+            break;
+        default:
+            cout << "Goodbye, World!" << endl;
+            break;
+    }
+    return 0;
+}
