bb059b9fe6070c5e6698ab9750b49efac21ec3f1
XaviCast0409 <168607424+XaviCast0409@users.noreply.github.com>
2026-07-31 17:43:26 -0500

    ejercicios

---
+++ ejercicios/10ejercicio.cpp
@@ -0,0 +1,12 @@
+/* 
+Simulador de Extracción de Datos (ETL) con Límite de Memoria
+Escribe un programa que pida registros numéricos al usuario usando un bucle while. 
+La condición normal de salida será que el usuario digite el número "-1" (señal de fin de datos).
+
+Regla adicional: Debes llevar un contador de cuántos datos van ingresando.
+
+Protección: Si el contador llega a 5 (para simular el límite de la memoria), 
+el programa debe ignorar cualquier otro dato, imprimir un mensaje de "Buffer de memoria lleno. 
+Forzando detención del proceso" y romper la ejecución del bucle con un break. Muestra al final 
+el promedio de los datos válidos leídos.
+*/
